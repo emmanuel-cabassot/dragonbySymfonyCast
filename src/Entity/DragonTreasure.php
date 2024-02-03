d030d968a8f1ac858502a94bc5606bdb1b572339
emmanuel-cabassot <emmanuel.cabassot@hotmail.fr>
2024-02-03 15:15:50 +0100

    first commit

---
+++ src/Entity/DragonTreasure.php
@@ -0,0 +1,192 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Doctrine\Orm\Filter\BooleanFilter;
+use ApiPlatform\Doctrine\Orm\Filter\RangeFilter;
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Metadata\ApiFilter;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
+use App\Repository\DragonTreasureRepository;
+use Carbon\Carbon;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Serializer\Annotation\SerializedName;
+// the u() function creates Unicode strings
+use function Symfony\Component\String\u;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Constraints\GreaterThanOrEqual;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+#[ORM\Entity(repositoryClass: DragonTreasureRepository::class)]
+#[ApiResource(
+    shortName: 'Treasure',
+    description: 'A rare and valuable treasure.',
+    operations: [
+        new Get(),
+        new GetCollection(),
+        new Post(),
+        new Put(),
+        new Patch(),
+    ],
+    normalizationContext: [
+        'groups' => ['treasure:read'],
+    ],
+    denormalizationContext: [
+        'groups' => ['treasure:write'],
+    ],
+    paginationItemsPerPage: 10,
+    formats: [
+        'jsonld',
+        'csv' => 'text/csv',
+    ]
+)]
+class DragonTreasure
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    #[Groups(['treasure:read', 'treasure:write'])]
+    #[ApiFilter(SearchFilter::class, strategy: 'partial')]
+    #[Assert\NotBlank]
+    #[Assert\Length(min: 3, max:12)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    #[Groups(['treasure:read'])]
+    #[ApiFilter(SearchFilter::class, strategy: 'partial')]
+    #[Assert\NotBlank]
+    #[Assert\Length(min: 3)]
+    private ?string $description = null;
+
+    /**
+     * The estimated value of this treasure, in gold coins.
+     */
+    #[ORM\Column]
+    #[Groups(['treasure:read', 'treasure:write'])]
+    #[ApiFilter(RangeFilter::class)]
+    #[Assert\GreaterThanOrEqual(1)]
+    private ?int $value = 0;
+
+    #[ORM\Column]
+    #[Groups(['treasure:read', 'treasure:write'])]
+    #[Assert\GreaterThanOrEqual(0)]
+    #[Assert\LessThanOrEqual(10)]
+    private ?int $coolFactor = 0;
+
+    #[ORM\Column]
+    private \DateTimeImmutable $plunderedAt;
+
+    #[ORM\Column]
+    #[ApiFilter(BooleanFilter::class)]
+    private bool $isPublished = false;
+
+    public function __construct(string $name = null)
+    {
+        $this->name = $name;
+        $this->plunderedAt = new \DateTimeImmutable();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    #[Groups(['treasure:read'])]
+    public function getShortDescription(): ?string
+    {
+        return u($this->description)->truncate(40, ' ...');
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    #[SerializedName('description')]
+    #[Groups(['treasure:write'])]
+    public function setTextDescription(string $description): self
+    {
+        $this->description = nl2br($description);
+
+        return $this;
+    }
+
+    public function getValue(): ?int
+    {
+        return $this->value;
+    }
+
+    public function setValue(int $value): self
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+
+    public function getCoolFactor(): ?int
+    {
+        return $this->coolFactor;
+    }
+
+    public function setCoolFactor(int $coolFactor): self
+    {
+        $this->coolFactor = $coolFactor;
+
+        return $this;
+    }
+
+    public function getPlunderedAt(): ?\DateTimeImmutable
+    {
+        return $this->plunderedAt;
+    }
+
+    public function setPlunderedAt(\DateTimeImmutable $plunderedAt): self
+    {
+        $this->plunderedAt = $plunderedAt;
+
+        return $this;
+    }
+
+    /**
+     * A human-readable representation of when this treasure was plundered.
+     */
+    #[Groups(['treasure:read'])]
+    public function getPlunderedAtAgo(): string
+    {
+        return Carbon::instance($this->plunderedAt)->diffForHumans();
+    }
+
+    public function getIsPublished(): bool
+    {
+        return $this->isPublished;
+    }
+
+    public function setIsPublished(bool $isPublished): self
+    {
+        $this->isPublished = $isPublished;
+
+        return $this;
+    }
+}
